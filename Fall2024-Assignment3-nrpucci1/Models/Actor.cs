433c1321e3addd62ececd43bd4dc2a64fa546c08
npucci88 <nrpucci@crimson.ua.edu>
2024-10-29 14:26:48 -0500

    Create Movie and Actor model classes

---
+++ Fall2024-Assignment3-nrpucci1/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿namespace Fall2024_Assignment3_nrpucci1.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string ImdbLink { get; set; }
+        public string PhotoURL { get; set; }
+        public List<Movie> Movies { get; set; }
+
+    }
+}
---
+++ Fall2024-Assignment3-nrpucci1/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Fall2024_Assignment3_nrpucci1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string ImdbLink { get; set; }
+        public string Genre {  get; set; }
+        public string Title { get; set; }
+
+        public string YearOfRelease { get; set; }
+        public string PosterURL { get; set; }
+
+        public List<Actor> Actors { get; set; }
+    }
+}
