b953b7a95d51d00df557babb790ff852b6028128
npucci88 <nrpucci@crimson.ua.edu>
2024-10-28 23:56:16 -0500

    Add project files.

---
+++ Fall2024-Assignment3-nrpucci1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment3_nrpucci1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Fall2024-Assignment3-nrpucci1/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+// Set the fully constructed connection string
+builder.Services.AddDbContext < ApplicationDbContext > (options =>
+    options.UseSqlServer(connectionStringBuilder.ConnectionString));
+```
