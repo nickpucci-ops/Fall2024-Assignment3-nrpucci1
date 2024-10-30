71258dd0f027bab982d579215e9d5032ce0e4a8b
npucci88 <nrpucci@crimson.ua.edu>
2024-10-30 15:37:35 -0500

    Starting on movie/actor linking management page. Creating the MovieActor class and model

---
+++ Fall2024-Assignment3-nrpucci1/Models/MovieActor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Fall2024_Assignment3_nrpucci1.Models;
+
+namespace YourNamespace.Models
+{
+    public class MovieActor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Foreign Key to Movie
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        // Foreign Key to Actor
+        [ForeignKey("Actor")]
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
