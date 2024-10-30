e7fc9508bba884b4d21d6ac1eb304096b5d789a1
npucci88 <nrpucci@crimson.ua.edu>
2024-10-29 19:30:01 -0500

    Add OpenAI client, install and add services, VaderSharp2 for sentiment analyzing, generate ai reviews, calculate overall sentiment, Preparing view model. Create MovieDetailsViewModel class.

---
+++ Fall2024-Assignment3-nrpucci1/Controllers/MovieDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Fall2024_Assignment3_nrpucci1.Models
+{
+    public class MovieDetailsViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<(string Review, string Sentiment)> Reviews { get; set; }
+        public string OverallSentiment { get; set; }
+    }
+}
--- Fall2024-Assignment3-nrpucci1/Controllers/MoviesController.cs
+++ Fall2024-Assignment3-nrpucci1/Controllers/MoviesController.cs
@@ -2,156 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Fall2024_Assignment3_nrpucci1.Data;
 using Fall2024_Assignment3_nrpucci1.Models;
+using VaderSharp2; //used for the sentiment analysis
 
 namespace Fall2024_Assignment3_nrpucci1.Controllers
 {
     public class MoviesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly OpenAIClient _openAiClient;
+        private readonly SentimentIntensityAnalyzer _sentimentAnalyzer;
 
         public MoviesController(ApplicationDbContext context)
         {
             _context = context;
-        }
 
-        // GET: Movies
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Movies.ToListAsync());
+            //OpenAI client (replace with actual endpoint and key from secrets)
+            string endpoint = "https://your-openai-resource-name.openai.azure.com/";
+            string apiKey = "Azure_OpenAI_Key";
+            _openAiClient = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+            //sentiment analyzer initialization
+            _sentimentAnalyzer = new SentimentIntensityAnalyzer();
         }
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var movie = await _context.Movies
+                .Include(m => m.Actors) //include related actors if applicable
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (movie == null)
+
+            if (movie == null) return NotFound();
+
+            //generates ai reviews
+            var reviews = new List<(string Review, string Sentiment)>();
+            for (int i = 0; i < 10; i++)
             {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        // GET: Movies/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Movies/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ImdbLink,Genre,Title,YearOfRelease,PosterURL")] Movie movie)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(movie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
-        }
-
-        // GET: Movies/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(movie);
-        }
-
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ImdbLink,Genre,Title,YearOfRelease,PosterURL")] Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                var response = await _openAiClient.GetChatCompletionsAsync("gpt-35-turbo", new ChatCompletionsOptions
                 {
-                    _context.Update(movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.Id))
+                    Messages = new List<ChatMessage>
                     {
-                        return NotFound();
+                        new ChatMessage("user", $"Write a review for the movie '{movie.Title}'.")
                     }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
-        }
+                });
 
-        // GET: Movies/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
+                var reviewText = response.Value.Choices.First().Message.Content;
+
+                //analyze the sentiment of given review
+                var sentimentScore = _sentimentAnalyzer.PolarityScores(reviewText).Compound;
+                var sentiment = sentimentScore > 0 ? "Positive" : "Negative";
+
+                reviews.Add((reviewText, sentiment));
+            }
+
+            //calculate the overall sentiment of the reviews
+            var overallSentiment = reviews.Count(r => r.Sentiment == "Positive") > 5 ? "Positive" : "Negative";
+
+            //prepare view model
+            var viewModel = new MovieDetailsViewModel
             {
-                return NotFound();
-            }
+                Movie = movie,
+                Reviews = reviews,
+                OverallSentiment = overallSentiment
+            };
 
-            var movie = await _context.Movies
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
+            return View(viewModel);
         }
 
-        // POST: Movies/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie != null)
-            {
-                _context.Movies.Remove(movie);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool MovieExists(int id)
-        {
-            return _context.Movies.Any(e => e.Id == id);
-        }
+        
     }
 }
