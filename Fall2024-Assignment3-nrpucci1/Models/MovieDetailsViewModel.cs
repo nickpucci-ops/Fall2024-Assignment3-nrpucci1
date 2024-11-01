c2e9d079c671b164ea63ece329c74b3a6a6ed946
npucci88 <nrpucci@crimson.ua.edu>
2024-11-01 15:31:38 -0500

    Fixing possible null dereference in Movies Details

--- Fall2024-Assignment3-nrpucci1/Models/MovieDetailsViewModel.cs
+++ Fall2024-Assignment3-nrpucci1/Models/MovieDetailsViewModel.cs
@@ -4,9 +4,9 @@ namespace Fall2024_Assignment3_nrpucci1.Models;
 public class MovieDetailsViewModel
 {
     public Movie Movie { get; set; }
-    public IEnumerable<Actor> Actors { get; set; }
+    public IEnumerable<Actor>? Actors { get; set; } = new List<Actor>();
     public string OverallSentiment { get; set; } = string.Empty;
-    public List<(string Review, string Sentiment)> Reviews { get; set; }
+    public List<(string Review, string Sentiment)>? Reviews { get; set; } = new List<(string, string)>();
     public MovieDetailsViewModel(Movie movie)
     {
         Movie = movie;
