4b9e55caebb69ec74bf61ff3585a59f356e83009
npucci88 <nrpucci@crimson.ua.edu>
2024-10-31 21:31:51 -0500

    Deleting MovieActor stuff to rescaffold everything

--- Fall2024-Assignment3-nrpucci1/Models/ActorDetailsViewModel.cs
+++ Fall2024-Assignment3-nrpucci1/Models/ActorDetailsViewModel.cs
@@ -9,6 +9,7 @@ public class ActorDetailsViewModel
         Actor = actor;
         Movies = movies;
     }
+
     public List<(string Tweet, string Sentiment)> Tweets { get; set; } = new List<(string, string)>();
     public string OverallSentiment { get; set; } = string.Empty;
 }
